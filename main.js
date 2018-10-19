16f847d2e514b34fcc33eb88c33071e3d85f2c4d
Carrier.Zhang <carrier.zhang@ef.com>
2018-10-19 23:26:48 +0800

    modules dependency feature is done.

--- main.js
+++ main.js
@@ -9,33 +9,24 @@
      * @api public
      */
 
-    function require(path) {
-        var resolved = require.resolve(path);
-
+    function require(p) {
+        var path = require.resolve(p);
+        //register时候 已经在modules里添加了 N个模块 a.js/b.js/c.js
+        var module = require.modules[path];
         // lookup failed
-        if (null == resolved) {
-            throw new Error('Failed to require');
-            return
+        if (!module) {
+            throw new Error('failed to require ' + path);
         }
 
-        var module = require.modules[resolved];
+        if (!module.exports) {
+            // perform real require()
+            // by invoking the module's
+            // registered function
+            var mod = {};
+            module.call(this, mod, null, require.relative(path));
+            module.exports = mod.exports;
+        }
 
-        // perform real require()
-        // by invoking the module's
-        // registered function
-        // if (!module._resolving && !module.exports) {
-        //     var mod = {};
-        //     mod.exports = {};
-        //     mod.client = mod.component = true;
-        //     module._resolving = true;
-        //     module.call(this, mod.exports, require.relative(resolved), mod);
-        //     delete module._resolving;
-        //     module.exports = mod.exports;
-        // }
-        var mod = {};
-        mod.exports = {};
-        module.call(this, mod.exports, null, mod);
-        module.exports = mod.exports.myExports;
         return module.exports;
     }
 
@@ -76,24 +67,35 @@
         };
     };
 
-    require.register("browser/debug.js", function(module, exports, require) {
-        //why need a new property?
-        module.myExports = {
+    require.register("a.js", function(module, exports, require) {
+        //采用 CommonJS 模块规范
+        module.exports = {
+            message: "hello world",
             test: function() {
-                console.log("test function run");
+                var b = require("b");
+                var c = require("c");
+                console.log('message inside : ' + this.message);
+                console.log('b : ' + b.SimpleMessage);
+                console.log('c : ' + c.NewMessage);
             }
         }
 
-    }); // module: browser/debug.js
+    }); // module: a.js
 
-    require.register("browser/fs.js", function(module, exports, require) {
+    require.register("b.js", function(module, exports, require) {
+        module.exports = {
+            SimpleMessage: 'simple world'
+        }
+    }); // module: b.js
 
-    }); // module: browser/fs.js
+    require.register("c.js", function(module, exports, require) {
+        module.exports = {
+            NewMessage: 'new world'
+        }
+    }); // module: c.js
 
-    require.register("browser/path.js", function(module, exports, require) {
+    var a = require("a");
+    a.test();
 
-    }); // module: browser/path.js
 
-    var myDebug = require("browser/debug");
-    myDebug.test();
 })(); //run immedately
